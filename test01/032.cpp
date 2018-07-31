42c6c784d0541bc34e25fec0e7c0a2dc6479e8a5
zhangjie <1466905773@qq.com>
2018-07-31 20:00:34 +0800

    test01/

--- test01/032.cpp
+++ test01/032.cpp
@@ -26,7 +26,8 @@ using namespace std;
 //public :
 //    Date()
 //    {}
-//    //拷贝构造函数
+//    //拷贝构造函数     一个构造函数的重载   必须采用传引用传参
+//
 //    Date(const Date& d)
 //    {
 //        _year=d._year;
@@ -40,12 +41,15 @@ using namespace std;
 //};
 //void Test()
 //{
-//    Date d;
+//    Date d1;
+//    Date d2=d1;//调用拷贝构造函数
 //}
+
 
 //析构函数
 //class Date
-//{
+//{i
+//public:
 //    ~Date()
 //    {}
 //private:
@@ -55,16 +59,25 @@ using namespace std;
 //};
 
 
-//赋值操作符的重载
+//赋值操作符的重载是对一个已经存在的对象进行拷贝复制
 //Date& operator = (const Date& d)
 //{
-//    if(this!&d)
+//    if(this!=&d)
 //    {
 //        this->_year=d._year;
 //        this->_month=d._momth;
 //        this->_day=d._day;
 //    }
 //    return *this;
+//}
+//void Test()
+//{
+// Date d1;
+// Date d2=d1;//调用拷贝构造函数 因为在创建d2对象
+//
+// Date d3;
+// d3=d1;
+//调用赋值运算符的重载    因为d3已经已经存在
 //}
 
 
